0f0d4465822ab36cce1fd585f8f3933c2a2ab840
DolphieDude <andreycherevat@gmail.com>
2019-11-23 14:37:25 +0200

    user class

---
+++ src/com/cherevatyi/User.java
@@ -0,0 +1,18 @@
+package com.cherevatyi;
+
+import org.graalvm.compiler.lir.LIRInstruction;
+
+public class User {
+    String name;
+    int age;
+    public User() { }
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+}
